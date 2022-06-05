34a42d8b8f4f1fad1d5bc1513cfbd9b2e3c0499b
Леонид <leonis13579@gmail.com>
2022-06-05 14:46:23 +0300

    Upgrading auth page

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,21 +1,36 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:repiton/provider/root_provider.dart';
 
-class AuthScreen extends StatelessWidget {
-  AuthScreen({Key? key}) : super(key: key);
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({Key? key}) : super(key: key);
 
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
   final _formKey = GlobalKey<FormState>();
+
   String login = "";
   String password = "";
 
-  void _authButtonPressed() {
+  bool isObscure = true;
+
+  Future<void> _authButtonPressed(BuildContext context) async {
     if (!_formKey.currentState!.validate()) return;
+
+    var result = false;
 
     _formKey.currentState!.save();
     if (login.contains("@")) {
-      RootProvider.getAuth().auth(null, login, password);
+      result = await RootProvider.getAuth().auth(null, login, password);
     } else {
-      RootProvider.getAuth().auth(login, null, password);
+      result = await RootProvider.getAuth().auth(login, null, password);
+    }
+
+    if (!result) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Ошибка входа")));
     }
   }
 
@@ -56,21 +71,39 @@ class AuthScreen extends StatelessWidget {
                               validator: (value) =>
                                   value == null || value.isEmpty ? "Введите корректный логин или почту" : null,
                               onSaved: (value) => login = value ?? "",
+                              textInputAction: TextInputAction.next,
                             ),
                             const SizedBox(height: 10),
-                            TextFormField(
-                              style: const TextStyle(fontSize: 18),
-                              obscureText: true,
-                              decoration: const InputDecoration(
-                                labelText: "Пароль",
-                              ),
-                              validator: (value) =>
-                                  value == null || value.length <= 5 ? "Введите корректный пароль" : null,
-                              onSaved: (value) => password = value ?? "",
+                            Row(
+                              children: [
+                                Expanded(
+                                  child: TextFormField(
+                                    style: const TextStyle(fontSize: 18),
+                                    obscureText: isObscure,
+                                    decoration: const InputDecoration(
+                                      labelText: "Пароль",
+                                    ),
+                                    validator: (value) =>
+                                        value == null || value.length <= 5 ? "Введите корректный пароль" : null,
+                                    onSaved: (value) => password = value ?? "",
+                                  ),
+                                ),
+                                IconButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      isObscure = !isObscure;
+                                    });
+                                  },
+                                  icon: Icon(
+                                    isObscure ? CupertinoIcons.eye_fill : CupertinoIcons.eye_slash_fill,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ],
                             ),
                             const SizedBox(height: 20),
                             ElevatedButton(
-                              onPressed: _authButtonPressed,
+                              onPressed: () => _authButtonPressed(context),
                               style: ElevatedButton.styleFrom(
                                 textStyle: const TextStyle(fontSize: 18),
                                 padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
