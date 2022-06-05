1a29ec1db8bca04bd5f409078a475a8b4576ab8f
Леонид <leonis13579@gmail.com>
2022-06-05 14:42:44 +0300

    Compressing user info func

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -8,7 +8,7 @@ import 'package:repiton/provider/root_provider.dart';
 class SettingsScreen extends ConsumerWidget {
   const SettingsScreen({Key? key}) : super(key: key);
 
-  Widget getUserNameAndRole<T>(Future<T> futureAction, String Function(T) userName, String userRole) {
+  Widget _getUserNameAndRole<T>(Future<T> futureAction, String Function(T) userName, String userRole) {
     return FutureBuilder<T>(
       future: futureAction,
       builder: (context, snapshot) {
@@ -41,6 +41,42 @@ class SettingsScreen extends ConsumerWidget {
         }
       },
     );
+  }
+
+  Widget _getUserNameAndRoleFromRole(String userRole) {
+    if (userRole == AuthProvider.teacherRole) {
+      return _getUserNameAndRole<Teacher>(
+        RootProvider.getTeachers().getCachedTeacher(),
+        (teacher) {
+          return teacher.lastName + " " + teacher.name + " " + teacher.fatherName;
+        },
+        "Преподаватель",
+      );
+    } else if (userRole == AuthProvider.adminRole) {
+      // TODO: Change to admin provider
+      return _getUserNameAndRole<Teacher>(
+        RootProvider.getTeachers().getCachedTeacher(),
+        (teacher) {
+          return teacher.lastName + " " + teacher.name + " " + teacher.fatherName;
+        },
+        "Администратор",
+      );
+    } else if (userRole == AuthProvider.studentRole) {
+      return _getUserNameAndRole<Student>(
+        RootProvider.getStudents().getCachedStudent(),
+        (student) {
+          return student.lastName + " " + student.name;
+        },
+        "Ученик",
+      );
+    } else {
+      debugPrint(userRole[0]);
+      return Container();
+    }
+  }
+
+  void _logout() {
+    RootProvider.getAuth().logout();
   }
 
   @override
@@ -84,37 +120,12 @@ class SettingsScreen extends ConsumerWidget {
               ),
               child: Column(
                 children: [
-                  (() {
-                    if (_auth.userRole == AuthProvider.teacherRole) {
-                      return getUserNameAndRole<Teacher>(
-                        RootProvider.getTeachers().getCachedTeacher(),
-                        (teacher) {
-                          return teacher.lastName + " " + teacher.name + " " + teacher.fatherName;
-                        },
-                        "Преподаватель",
-                      );
-                    } else if (_auth.userRole == AuthProvider.adminRole) {
-                      // TODO: Change to admin provider
-                      return getUserNameAndRole<Teacher>(
-                        RootProvider.getTeachers().getCachedTeacher(),
-                        (teacher) {
-                          return teacher.lastName + " " + teacher.name + " " + teacher.fatherName;
-                        },
-                        "Администратор",
-                      );
-                    } else if (_auth.userRole == AuthProvider.studentRole) {
-                      return getUserNameAndRole<Student>(
-                        RootProvider.getStudents().getCachedStudent(),
-                        (student) {
-                          return student.lastName + " " + student.name;
-                        },
-                        "Ученик",
-                      );
-                    } else {
-                      debugPrint(_auth.userRole[0]);
-                      return Container();
-                    }
-                  }())
+                  _getUserNameAndRoleFromRole(_auth.userRole),
+                  Expanded(child: Container()),
+                  TextButton(
+                    onPressed: _logout,
+                    child: Text("Выход"),
+                  ),
                 ],
               ),
             ),
