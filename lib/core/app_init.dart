e163808f33761c57a42b4d6cf09778493bdb68bd
Леонид <leonis13579@gmail.com>
2022-06-13 16:09:00 +0300

    Set saved token to rest api client on init

--- lib/core/app_init.dart
+++ lib/core/app_init.dart
@@ -5,9 +5,10 @@ import 'package:repiton/core/tokenStorage/secure_token_storage.dart';
 Future<void> preInit() async {
   GetIt.I.allowReassignment = true;
 
-  final restService = await RepitonApiContainer.init("https://backend.repiton.dev.realityfamily.ru:9046/api/v1/");
-  GetIt.I.registerSingleton<RepitonApiContainer>(restService);
-
   final tokenStorage = await SecureTokenStorage.init();
   GetIt.I.registerSingleton<SecureTokenStorage>(tokenStorage);
+
+  final restService = await RepitonApiContainer.init("https://backend.repiton.dev.realityfamily.ru:9046/api/v1/");
+  if (tokenStorage.authToken != null) restService.setToken(tokenStorage.authToken!);
+  GetIt.I.registerSingleton<RepitonApiContainer>(restService);
 }
